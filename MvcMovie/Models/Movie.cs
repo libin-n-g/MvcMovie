e980b0b5193e47944e45cba122e0393c69d8ddb8
libin-n-g <libinngeorge@gmail.com>
2018-05-05 18:56:55 +0530

    Adds Validations in the model

--- MvcMovie/Models/Movie.cs
+++ MvcMovie/Models/Movie.cs
@@ -14,13 +14,21 @@ namespace MvcMovie.Models
     public class Movie
     {
         public int ID { get; set; }
+
+        [StringLength(60,MinimumLength = 3), Required]
         public string Title { get; set; }
 
-        [Display(Name = "Release Date")]
-        [DataType(DataType.Date)]
+        [DisplayFormat(DataFormatString = "{0:dd-MM-yyyy}", ApplyFormatInEditMode = true)]
+        [Display(Name = "Release Date"), DataType(DataType.Date)]
         public DateTime ReleaseDate { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), Required, StringLength(30)]
         public string Genre { get; set; }
+
+        [Range(1, 100), DataType(DataType.Currency), Required]
         public decimal Price { get; set; }
+
+        [RegularExpression(@"^[A-Z]+[a-zA-Z""'\s-]*$"), StringLength(5), Required]
         public string Rating { get; set; }
     }
 }
